2ad36b44503864fd4d003d7f4d6dd3525384d8bb
kartheek <karthikmarabathula18@gmail.com>
2026-06-20 16:11:10 +0530

    updated mdefault workspace code

--- ec2.tf
+++ ec2.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "sg_ec2" {
 resource "aws_instance" "my_instance" {
     for_each = tomap({
         terra-automate-ec2-small = "t3.small"
-        terra-automate-ec2-small-2 = "t3.small"
+        terra-automate-ec2-micro = "t3.micro"
     })
     instance_type = each.value
     key_name = aws_key_pair.key_pair_ec2.key_name
