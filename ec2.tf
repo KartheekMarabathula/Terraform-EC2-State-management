c1d473c07d53895625eafb28693832bb89d52339
kartheek <karthikmarabathula18@gmail.com>
2026-06-20 16:08:04 +0530

    check prod code

--- ec2.tf
+++ ec2.tf
@@ -39,7 +39,6 @@ resource "aws_security_group" "sg_ec2" {
 
 resource "aws_instance" "my_instance" {
     for_each = tomap({
-        terra-automate-ec2-small = "t3.small"
         terra-automate-ec2-micro = "t3.micro"
     })
     instance_type = each.value
